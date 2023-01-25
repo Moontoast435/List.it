c34a83787ca79b46d985b553aad02550d2dc6ea2
Moontoast435 <seanmalone1993@gmail.com>
2023-01-25 19:07:18 +0000

    Add basic app layout

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -3,6 +3,10 @@ import './style.css';
 function layout() {
     const mainContainer = document.createElement('div');
     mainContainer.classList = 'main';
+    const navBar = document.createElement('div');
+    navBar.classList = 'navbar';
+
+    mainContainer.appendChild(navBar);
     
     return mainContainer;
 }
