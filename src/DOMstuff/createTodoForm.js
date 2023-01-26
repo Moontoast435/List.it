9c61b435a409c773b4ab12a7ebbd732016d0c877
Moontoast435 <seanmalone1993@gmail.com>
2023-01-26 17:48:11 +0000

    Add form elements

--- src/DOMstuff/createTodoForm.js
+++ src/DOMstuff/createTodoForm.js
@@ -13,14 +13,23 @@ const createForm = () => {
         const formContainer = document.createElement('div');
         formContainer.classList = 'form-container';
 
-        const createTodoForm = document.createElement('form');
-        formContainer.appendChild(createTodoForm);
-
+        const form =   `<form>
+                        <h2> Create your todo here!</h2>
+                        <label for="todo-title"> Todo title: </label>
+                        <input type="text" name="todo-data-title id="todo-title">
+                        <label for="todo-description"> Todo description: </label>
+                        <textarea></textarea>
+                        <label> Todo deadline: </label>
+                        <input type="date" name="todo-data-deadline" id="todo-date"> 
+                        </form>`;
+        formContainer.innerHTML = form;
+    
         overlay.appendChild(formContainer);
 
         overlay.classList = 'modal-overlay';
         overlay.classList.add('modal-visible');
         document.body.appendChild(overlay);
+
     } else {
         toggleForm();
     }
