62e769cab22dbc096699ca54cb7c2ccda6c17149
Moontoast435 <seanmalone1993@gmail.com>
2023-02-24 18:29:37 +0000

    Write tests for more elements

--- test/layout.test.js
+++ test/layout.test.js
@@ -13,3 +13,23 @@ test('the navbar is rendered', () => {
     let navbar = document.getElementsByClassName('navbar')[0];
     expect(navbar).toBeTruthy();
 })
+
+test('There is a default project ', () => {
+    let project = document.getElementsByClassName('project-button')[0];
+    expect(project).toBeTruthy();
+})
+
+test('The default project says Project 1', () => {
+    let projectText = document.getElementsByClassName('project-button')[0].textContent
+    expect(projectText).toBe('Project 1');
+})
+
+test('add project button is rendered', () => {
+    let addProjectButton = document.getElementsByClassName('navbar')[0];
+    expect(addProjectButton).toBeTruthy();
+})
+
+test('the navbar is rendered', () => {
+    let navbar = document.getElementsByClassName('navbar')[0];
+    expect(navbar).toBeTruthy();
+})
