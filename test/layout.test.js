16f68516ea934093361bc48328fdee1ef36d826b
Moontoast435 <seanmalone1993@gmail.com>
2023-02-24 18:21:30 +0000

    Write test to check for navbar

--- test/layout.test.js
+++ test/layout.test.js
@@ -8,3 +8,8 @@ test('the main container is rendered', () => {
     let mainContainer = document.getElementsByClassName('main')[0]
     expect(mainContainer).toBeTruthy();
 })
+
+test('the navbar is rendered', () => {
+    let navbar = document.getElementsByClassName('navbar')[0];
+    expect(navbar).toBeTruthy();
+})
